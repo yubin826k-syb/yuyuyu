d19ec2c874509987e9fb2eec5b5f4fb255eaa11c
yubin826k-syb <yubin826k@gmail.com>
2026-07-07 08:50:58 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,21 @@
-// Firebase 콘솔에서 만든 웹 앱 설정값을 아래에 붙여넣으세요.
-// (README.md의 "Firebase 설정하기" 단계를 먼저 따라해주세요)
-window.FIREBASE_CONFIG = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBqU0QfMCAYMD3k7DNN2vj2A42t_tqM9PM",
+  authDomain: "gogo-ac579.firebaseapp.com",
+  projectId: "gogo-ac579",
+  storageBucket: "gogo-ac579.firebasestorage.app",
+  messagingSenderId: "89900821714",
+  appId: "1:89900821714:web:bccc6cf4f148759d05910b",
+  measurementId: "G-LY0QPE7N80"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
