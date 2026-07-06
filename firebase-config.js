6994cf16fdffd0224ec8d52501a33fb6fe10ba93
yubin826k-syb <yubin826k@gmail.com>
2026-07-07 08:50:13 +0900

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Firebase 콘솔에서 만든 웹 앱 설정값을 아래에 붙여넣으세요.
+// (README.md의 "Firebase 설정하기" 단계를 먼저 따라해주세요)
+window.FIREBASE_CONFIG = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
