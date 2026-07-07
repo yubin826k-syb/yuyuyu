aa129cbcf37ac917f19b2d0106dcc8a1bbe78d0c
yubin826k-syb <yubin826k@gmail.com>
2026-07-07 16:53:19 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
 window.FIREBASE_CONFIG = {
   apiKey: "AIzaSyBqU0QfMCAYMD3k7DNN2vj2A42t_tqM9PM",
   authDomain: "gogo-ac579.firebaseapp.com",
@@ -15,7 +6,3 @@ window.FIREBASE_CONFIG = {
   messagingSenderId: "89900821714",
   appId: "1:89900821714:web:bccc6cf4f148759d05910b"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
