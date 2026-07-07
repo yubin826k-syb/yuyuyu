17813abea560b05a2c942ab1b5eabae6dcb9b660
yubin826k-syb <yubin826k@gmail.com>
2026-07-07 16:49:30 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
+window.FIREBASE_CONFIG = {
   apiKey: "AIzaSyBqU0QfMCAYMD3k7DNN2vj2A42t_tqM9PM",
   authDomain: "gogo-ac579.firebaseapp.com",
   projectId: "gogo-ac579",
   storageBucket: "gogo-ac579.firebasestorage.app",
   messagingSenderId: "89900821714",
-  appId: "1:89900821714:web:bccc6cf4f148759d05910b",
-  measurementId: "G-LY0QPE7N80"
+  appId: "1:89900821714:web:bccc6cf4f148759d05910b"
 };
 
 // Initialize Firebase
